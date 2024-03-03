d86977556c226a7bce27f592818389519dc74bf9
João V. Santos <joaovaledosantos@gmail.com>
2024-03-03 18:17:07 -0300

    Projeto springboot-challengers

---
+++ src/main/java/br/com/springger/challengers/SpringbootChallengersApplication.java
@@ -0,0 +1,13 @@
+package br.com.springger.challengers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootChallengersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootChallengersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/springger/challengers/SpringbootChallengersApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.springger.challengers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootChallengersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
